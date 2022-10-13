d05662a98e14a725fb54c038e861a7b9ae5a943f
Navarro Pierre <pierre_navarro@hotmail.fr>
2022-10-13 23:20:19 +0200

    img

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon, Types } from 'src/app/services/pokemon/element';
 import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
 
 @Component({
@@ -10,15 +11,31 @@ export class OverviewComponent implements OnInit {
 
   constructor(private pokemonService: PokemonService) { }
 
+  pokemons: Array<Pokemon> = [];
+
   ngOnInit(): void {
     this.refresh();
   }
 
   refresh(): void {
     this.pokemonService.getPokemons().subscribe(pokemons => {
-      pokemons.forEach(pokemon => {
-        console.log(pokemon);
-      });
+      this.pokemons = pokemons;
     })
+  }
+
+  getName(pokemon: Pokemon) {
+    return pokemon.name;
+  }
+
+  getTypes(pokemon: Pokemon) {
+    return pokemon.types.join('/');
+  }
+
+  getBackgroundColor(pokemon: Pokemon) {
+    if (pokemon.types.length === 1) {
+      return {'backgroundColor': Types.get(pokemon.types[0])?.color };
+    } else {
+      return {'background-image': `linear-gradient(${Types.get(pokemon.types[0])?.color}, ${Types.get(pokemon.types[1])?.color})` };
+    }
   }
 }
--- src/app/services/pokemon/element.ts
+++ src/app/services/pokemon/element.ts
@@ -55,3 +55,29 @@ export interface Translation {
     fr?: string,
     en?: string
 }
+
+export interface Type {
+    translation: Translation,
+    color?: string
+}
+
+export const Types: Map<String, Type> = new Map([
+    ['fire',        {translation: {'fr': 'feu', 'en': 'fire'}, color:'#F39B5A'}] ,
+    ['grass',       {translation: {'fr':'plante', 'en':'grass'}, color:'#78C850'}],
+    ['electric',    {translation: {'fr':'électrique', 'en':'electric'}, color:'#FADB5F'}],
+    ['water',       {translation: {'fr':'eau', 'en':'water'}, color:'#8EC8F3'}],
+    ['ground',      {translation: {'fr':'sol', 'en':'ground'}, color:'#E0C068'}],
+    ['rock',        {translation: {'fr':'roche', 'en':'rock'}, color:'#B8A038'}],
+    ['fairy',       {translation: {'fr':'fée', 'en':'fairy'}, color:'#EE99AC'}],
+    ['poison',      {translation: {'fr':'poison', 'en':'poison'}, color:'#CD81CD'}],
+    ['bug',         {translation: {'fr':'insecte', 'en':'bug'}, color:'#A8B820'}],
+    ['dragon',      {translation: {'fr':'dragon', 'en':'dragon'}, color:'#9A72FA'}],
+    ['psychic',     {translation: {'fr':'psy', 'en':'psychic'}, color:'#FA749C'}],
+    ['flying',      {translation: {'fr':'vol', 'en':'flying'}, color:'#D3C7F7'}],
+    ['fighting',    {translation: {'fr':'combat', 'en':'fighting'}, color:'#D95048'}],
+    ['normal',      {translation: {'fr':'normal', 'en':'normal'}, color:'#A8A878'}],
+    ['dark',        {translation: {'fr':'ténèbre', 'en':'dark'}, color:'#808B96'}],
+    ['ice',         {translation: {'fr':'glace', 'en':'ice'}, color:'#98D8D8'}],
+    ['ghost',       {translation: {'fr':'spectre', 'en':'ghost'}, color:'#705898'}],
+    ['steel',       {translation: {'fr':'acier', 'en':'steel'}, color:'#B8B8D0'}],
+]);
